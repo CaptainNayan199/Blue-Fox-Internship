a003a23564deba55b7737ab6fac569241057185d
Captainnayan199 <nayanpathak233199@gmail.com>
2024-10-29 18:07:16 +0545

    New flutter project created

--- Test/test.py
+++
@@ -1,2 +0,0 @@
-# sdvbsdjv
-print("sdvsdsd")
---
+++ broker_app/lib/main.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('AppBar'),
+          centerTitle: true,
+        ),
+        body: const Center(),
+      ),
+    );
+  }
+}
