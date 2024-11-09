4d5c0cd1f30ed4b928e99d60d8261fe68277ecdf
Captainnayan199 <nayanpathak233199@gmail.com>
2024-11-09 21:05:08 +0545

    Home page UI changed

--- broker_app/lib/PropSearch_HomeServices/container.dart
+++ broker_app/lib/PropSearch_HomeServices/container.dart
@@ -2,90 +2,22 @@
 
 import 'package:flutter/material.dart';
 
-class PropertySearchPage extends StatefulWidget {
-  const PropertySearchPage({super.key});
-
-  @override
-  _PropertySearchPageState createState() => _PropertySearchPageState();
-}
-
-class _PropertySearchPageState extends State<PropertySearchPage> {
-  // Track the selected tab (default to 0 for "Property Search")
-  int _selectedTabIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          // Rounded rectangular container with the tabs
-          Container(
-            margin: EdgeInsets.all(4), // Add some margin around the container
-            padding: EdgeInsets.symmetric(
-                vertical: 2), // Padding for vertical spacing
-            decoration: BoxDecoration(
-              color: Colors.grey[200], // Background color of the container
-              borderRadius: BorderRadius.circular(14), // Rounded corners
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                // Property Search Tab
-                _buildTabItem(
-                  index: 0,
-                  title: "Property Search",
-                  isSelected: _selectedTabIndex == 0,
-                ),
-                // Home Services Tab
-                _buildTabItem(
-                  index: 1,
-                  title: "Home Services",
-                  isSelected: _selectedTabIndex == 1,
-                ),
-              ],
-            ),
-          ),
-          // Placeholder for the body content of the selected tab
-          Expanded(
-            child: _selectedTabIndex == 0
-                ? PropertySearchContent()
-                : HomeServicesContent(),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // Method to build each tab item
-  Widget _buildTabItem(
-      {required int index, required String title, required bool isSelected}) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _selectedTabIndex = index; // Update selected tab
-        });
-      },
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 14, vertical: 6),
-        decoration: BoxDecoration(
-          color: isSelected ? Colors.red : Colors.white,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Text(
-          title,
-          style: TextStyle(
-            color: isSelected ? Colors.white : Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-// Placeholder widgets for tab content
-class PropertySearchContent extends StatelessWidget {
+class PropertySearchContent extends StatefulWidget {
   const PropertySearchContent({super.key});
+
+  @override
+  _PropertySearchContentState createState() => _PropertySearchContentState();
+}
+
+class _PropertySearchContentState extends State<PropertySearchContent> {
+  // Track the selected tab (default to 1 for "Rent" as the middle tab)
+  int _selectedIndex = 1; // Default to middle tab (Rent)
+
+  final List<Widget> _pages = [
+    Center(child: Text('Buy Page')),
+    Center(child: Text('Rent Page')),
+    Center(child: Text('Commercial Page')),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -100,7 +32,7 @@ class PropertySearchContent extends StatelessWidget {
                 children: [
                   TextSpan(
                     text: "100% Owner Properties |",
-                    style: TextStyle(color: Colors.black54, fontSize: 19),
+                    style: TextStyle(color: Colors.black54, fontSize: 18),
                   )
                 ],
               ),
@@ -110,18 +42,120 @@ class PropertySearchContent extends StatelessWidget {
                 children: [
                   TextSpan(
                     text: " Zero Brokerage",
-                    style: TextStyle(color: Colors.black, fontSize: 19),
+                    style: TextStyle(color: Colors.black, fontSize: 18),
                   )
                 ],
               ),
             ),
           ],
         ),
+        SizedBox(height: 15), // Space between the text and tabs
+
+        // Row for the 3 tabs
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildTab('Buy', 0),
+                  _buildTab('Rent', 1),
+                  _buildTab('Commercial', 2),
+                ],
+              ),
+              SizedBox(height: 20), // Space between the tabs and the search bar
+
+              // This is the search bar directly below the tabs
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 3.0),
+                child: TextField(
+                  // controller: controller,
+                  decoration: InputDecoration(
+                    // Placeholder centered
+                    hintText: 'Search up to 3 Localities or Landmarks',
+                    hintStyle: TextStyle(color: Colors.black54, fontSize: 13),
+
+                    // Border configuration
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black54),
+                      borderRadius:
+                          BorderRadius.circular(4.0), // Rounded corners
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black54),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black54),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+
+                    // Trailing search icon inside red container
+                    suffixIcon: Container(
+                      margin: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          // You can add your search action here
+                        },
+                        icon: Icon(Icons.search, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+
+        SizedBox(height: 20), // Space between the search bar and content
+
+        // Wrap the content inside a Container with a fixed height
         SizedBox(
-          height: 30,
+          height: 200, // You can adjust this value as per your need
+          child: _pages[_selectedIndex],
         ),
       ],
     );
+  }
+
+  // Method to build each tab item
+  Widget _buildTab(String title, int index) {
+    bool isSelected = _selectedIndex == index;
+    return GestureDetector(
+      onTap: () => _onTabSelected(index),
+      child: Container(
+        width: 100, // Fixed width for each tab
+        padding: EdgeInsets.symmetric(vertical: 10),
+        decoration: BoxDecoration(
+          border: isSelected
+              ? Border(
+                  bottom: BorderSide(color: Colors.red, width: 2),
+                )
+              : null,
+        ),
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+              color: isSelected ? Colors.red : Colors.black,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onTabSelected(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
 
@@ -131,94 +165,5 @@ class HomeServicesContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(child: Text("Home Services Content"));
-  }
-}
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 1; // Default selected tab is the middle one (Rent)
-
-  // Pages corresponding to each tab
-  final List<Widget> _pages = [
-    Center(child: Text('Buy Page')),
-    Center(child: Text('Rent Page')),
-    Center(child: Text('Commercial Page')),
-  ];
-
-  void _onTabSelected(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Tabs Row
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _buildTab('Buy', 0),
-            _buildTab('Rent', 1),
-            _buildTab('Commercial', 2),
-          ],
-        ),
-        // Page view (based on selected tab)
-        Expanded(
-          child: _pages[_selectedIndex],
-        ),
-      ],
-    );
-  }
-
-  Widget _buildTab(String title, int index) {
-    bool isSelected = _selectedIndex == index;
-    return Expanded(
-      child: GestureDetector(
-        onTap: () => _onTabSelected(index),
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          decoration: BoxDecoration(
-            border: isSelected
-                ? Border(
-                    bottom: BorderSide(color: Colors.red, width: 2),
-                  )
-                : null,
-          ),
-          child: Center(
-            child: Text(
-              title,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-                color: isSelected ? Colors.red : Colors.black,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
